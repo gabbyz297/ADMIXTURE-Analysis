9d822d1f68fa6d6c7f785260f049250a89a580b4
Gabrielle Atkinson <74087022+gabbyz297@users.noreply.github.com>
2024-11-30 13:54:06 -0600

    Update picard_readgroups.sh

--- picard_readgroups.sh
+++ picard_readgroups.sh
@@ -1,19 +1,15 @@
 #!/bin/bash
+#!/bin/bash
 #SBATCH -N 1
-#SBATCH -n 48
-#SBATCH -t 02:00:00
-#SBATCH -p workq
+#SBATCH -n 8
+#SBATCH -t 48:00:00
+#SBATCH -p single
 #SBATCH -A loni_chiguiro24
 #SBATCH -o picard_readgroup_112624.out
 #SBATCH -e picard_readgroup_112624.err
-
-
-module load parallel/20190222/intel-19.0.5
-
 
 cd /work/gabby297/bam_files/
 
 TMP_DIR=$PWD/tmp
 
-cat sample_list.txt | parallel "java -jar /project/sackettl/software/picard.jar AddOrReplaceReadGroups I={}.bam O={}tag.bam TMP_DIR=$PWD/tmp SO=coordinate RGID={}.bam RGLB=1 RGPL=illumina RGPU=1 RGSM={}.bam"
-~                                                           
+for i in /work/gabby297/bam_files/GA*.bam; do java -jar /project/sackettl/software/picard.jar AddOrReplaceReadGroups I=$i O=${i%.bam}.tag.bam MAX_RECORDS_IN_RAM=1000000 TMP_DIR=$PWD/tmp SO=coordinate RGID=${i%.bam} RGLB=1 RGPL=illumina RGPU=1 RGSM=${i%.bam}; done                                
