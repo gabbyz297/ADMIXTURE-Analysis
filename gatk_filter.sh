a2db9bbe9a8270f87a9568d8196bf6620879ecc1
Gabrielle Atkinson <74087022+gabbyz297@users.noreply.github.com>
2025-06-27 20:03:59 -0500

    Update gatk_filter.sh

--- gatk_filter.sh
+++ gatk_filter.sh
@@ -11,4 +11,4 @@ cd /work/gabby297/batch2/vcf_files/
 
 module load gatk/4.5.0.0
 
-gatk VariantFiltration --variant ALL_HAAM_final_SNP.vcf --output ALL_HAAM_final_SNP_filter.vcf   -R /project/sackettl/HAAM-from-OAAM-ref-genome/haam.consensus.called.from.oaam.alignment.V2.fasta --filter-name "ReadPosRankSum_filter" --filter-expression "ReadPosRankSum < -8.0" --filter-name "MQRankSum_filter" --filter-expression "MQRankSum < -12.5" --filter-name "FS_filter" --filter-expression "FS > 60.0" --filter-name "QD_filter" --filter-expression "QD < 2.0" --genotype-filter-name "DP8filter" --genotype-filter-expression "DP < 8" 2>/dev/null
+gatk VariantFiltration --variant ALL_HAAM_final_SNP.vcf --output ALL_HAAM_final_SNP_filter.vcf   -R /project/sackettl/HAAM-from-OAAM-ref-genome/haam.consensus.called.from.oaam.alignment.V2.fasta --filter-name "FS_filter" --filter-expression "FS > 60.0" --filter-name "QD_filter" --filter-expression "QD < 2.0" --genotype-filter-name "DP2filter" --genotype-filter-expression "DP < 2" 2>/dev/null
